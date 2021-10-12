cc2c790adbcec0de3ad9d47218fc628d75f1dfbd
Damian Tabor <zettie008@gmail.com>
2021-10-13 00:33:11 +0100

    Add test url pattern

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import home
+from .views import home, upload_img
 
 
 urlpatterns = [
     path('', home, name='home'),
+    path('convert/', upload_img),
 ]
