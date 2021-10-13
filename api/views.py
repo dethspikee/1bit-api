2c96ab172ade00d7722568ff191592134e2e5af7
Damian Tabor <zettie008@gmail.com>
2021-10-13 21:15:51 +0100

    Check if file was provided

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.utils.datastructures import MultiValueDictKeyError
 
 import PIL
 
@@ -17,14 +18,17 @@ def home(request):
 def upload_img(request):
     width = request.POST.get('width', 128)
     height = request.POST.get('height', 64)
-    image = request.FILES['file']
 
 
     try:
+        image = request.FILES['file']
         response = convert(image, (width, height))
         status = 200
     except PIL.UnidentifiedImageError as e:
         return JsonResponse({'error': 'Incorrect file contents (only images are allowed).'}, 
+                status=422)
+    except MultiValueDictKeyError as e:
+        return JsonResponse({'error': '"file" key missing. You must provide valid image file.'}, 
                 status=422)
 
     return JsonResponse({'payload': response}, status=status)
