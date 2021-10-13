0ec25e57174052b5fcfb5c22635cb37ad0bc293f
Damian Tabor <zettie008@gmail.com>
2021-10-13 21:12:32 +0100

    Check if image can be processed

--- api/views.py
+++ api/views.py
@@ -3,6 +3,8 @@ from django.views.decorators.http import require_POST
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
+import PIL
+
 from .converter import convert
 
 
@@ -16,6 +18,13 @@ def upload_img(request):
     width = request.POST.get('width', 128)
     height = request.POST.get('height', 64)
     image = request.FILES['file']
-    bytelist = convert(image, (width, height))
 
-    return JsonResponse({'payload': bytelist}, status=200)
+
+    try:
+        response = convert(image, (width, height))
+        status = 200
+    except PIL.UnidentifiedImageError as e:
+        return JsonResponse({'error': 'Incorrect file contents (only images are allowed).'}, 
+                status=422)
+
+    return JsonResponse({'payload': response}, status=status)
