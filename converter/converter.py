db8767431821c9b1c075f99b2ae74196f1995667
Damian Tabor <zettie008@gmail.com>
2021-10-14 22:22:29 +0100

    Add lru_cache decorator to cache previous computations. Accept another argument (threshold) to define black / whie pixel threshold during conversion.

--- converter/converter.py
+++ converter/converter.py
@@ -1,21 +1,34 @@
+from functools import lru_cache
+
 from PIL import Image
+from io import BytesIO
 import numpy as np
+import base64
 
 
-def convert(image, size):
+@lru_cache(maxsize=512)
+def convert(base64image, size, threshold=None):
     BYTE_SIZE = 8
     end = 8
     start = 0
 
+    image = BytesIO(base64.b64decode(base64image))
     width, height = int(size[0]), int(size[1])
-    img = Image.open(image)
-    img = img.convert('1', dither=Image.NONE).resize((width, height))
+    img = Image.open(image).resize((width, height))
+    if threshold is None:
+        img = img.convert('1', dither=Image.NONE)
+    else:
+        img = img.convert('L')
+        img = img.point(lambda x: 255 if x > int(threshold) else 0)
+        img.convert('1')
     arr = np.array(img, dtype=int)
 
     bytelist = []
 
     for row in range(height):
         for col in range(16):
+            if threshold is not None:
+                arr[row][arr[row] == 255] = 1
             bits = ''.join(str(bit) for bit in arr[row][start:end])
             if len(bits) != BYTE_SIZE:
                 bits += '0000'
