3db9a056574d8ab14f3aa3e262ca535ab48f2111
Damian Tabor <zettie008@gmail.com>
2021-10-14 22:23:24 +0100

    base64 encode image in order to use it in lru_cache. Retrieve threshold from params

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from bottle import route, run, post, request, response, abort
 
+from io import BytesIO
 from PIL import UnidentifiedImageError
+import base64
 
 from converter import convert
 
@@ -9,16 +11,16 @@ from converter import convert
 def handle_conversion():
     width = request.params.get('width', '128')
     height = request.params.get('height', '64')
-
+    threshold = request.params.get('threshold')
 
     try:
         image = request.files['file']
-        bytelist = convert(image.file, (width, height))
+        b64_image = base64.b64encode(image.file.read())
+        bytelist = convert(b64_image, (width, height), threshold)
     except UnidentifiedImageError:
         abort(422, 'Error processing file contents. Make sure you\'re sending valid image.')
     except KeyError:
         abort(422, "'file' key not found. Please provide image file.")
-
 
     return {'payload': bytelist}
 
