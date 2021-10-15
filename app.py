0bd3e19f87adcbef48bf26f968bcf9a76e852c8c
Damian Tabor <zettie008@gmail.com>
2021-10-15 22:46:36 +0100

    Check if PROD env variable is set otherwise run on localhost

--- app.py
+++ app.py
@@ -24,4 +24,8 @@ def handle_conversion():
     return {'payload': bytelist}
 
 
-run(server='gunicorn', host='0.0.0.0', port=int(os.environ.get('PORT')))
+if os.environ.get('PROD'):
+    run(server='gunicorn', host='0.0.0.0', port=int(os.environ.get('PORT')))
+else:
+    run(host='localhost', port=8080, debug=True, reloader=True)
+
