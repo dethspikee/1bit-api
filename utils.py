5bc5441ccffaf9a1ef4cdccab718128533e3692d
Damian Tabor <zettie008@gmail.com>
2021-10-17 23:52:25 +0100

    Make 'create_response' function more generic by accepting content type argument

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from bottle import HTTPResponse
 
 
-def create_response(body, status_code):
-    response = HTTPResponse({'error': body}, status_code)
-    response.set_header('Content-Type', 'application/json')
+def create_response(body, status_code, content_type):
+    response = HTTPResponse(body, status_code)
+    response.set_header('Content-Type', content_type)
     return response
